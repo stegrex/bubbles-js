9179c8e1c17eb25d0aa9a274b695cd314515a495
stegrex <stegrex@users.noreply.github.com>
2014-05-21 23:05:57 -0700

    2012-06-02a: Implemented bubble asplode when a large bubble combines into 15 weight.

--- bubble.js
+++ bubble.js
@@ -3,9 +3,11 @@
 var gameMode = 1;
 var bubbleRadius = 5; // Bubble radius. Default 5.
 var maxBubbles = 25; // Maximum number of bubbles allowed in the game.
+var maxBubbleWeight = 15; // Maximum weight of the bubble before it asplodes.
 var bubbleFrequency = 150; // Milliseconds in between firings. Default 250.
 var canvasX = 320;
 var canvasY = 480;
+var msPerRender = 4; // Milliseconds per animation rendering.
 
 // Global Variables
 
@@ -13,6 +15,7 @@ var game; // The game object. Handles game state.
 var canvas; // The canvas object. Handles rendering.
 var input; // The game's event object. Handles user input actions.
 var t; // Global timer (uses 1 ms increments). Handles rendering and state.
+var calcCount; // Global timer.
 
 var song; // Object for the song.
 
@@ -30,9 +33,6 @@ function main () // Initializes the game.
 	canvas.create();
 	input = new Input();
 	input.create();
-	song = new Audio("6-1-12.ogg");
-	song.loop = true;
-	document.getElementById("musicToggle").addEventListener("click", musicToggle, false);
 	t = setTimeout(redraw, 1); // Start the game's timer loop.
 }
 function redraw () // Redraws the canvas.
@@ -70,12 +70,16 @@ Game.prototype.create = function () // Load different components for the level.
 {
 	this.blocks = new Array();
 	this.bubbles = new Array();
+	this.asplodeBubble = null;
 	
 	this.load();
 	
 }
 Game.prototype.load = function () // Load the components of a level from a file.
 {
+	song = new Audio("6-1-12.ogg");
+	song.loop = true;
+	document.getElementById("musicToggle").addEventListener("click", musicToggle, false);
 	this.blocks[0] = new Block();
 	this.blocks[0].create(200, 200, 100, 10);
 	this.blocks[1] = new Block();
@@ -93,30 +97,49 @@ Game.prototype.destruct = function () // Destruct the game, restarting.
 function Canvas () // Canvas class.
 {
 	this.ctx;
+	this.calcCount;
 }
 Canvas.prototype.create = function () // Create method for the canvas (constructor).
 {
 	var canvas = document.getElementById("canvas");
 	this.ctx = canvas.getContext("2d");
+	this.calcCount = 0;
 	this.redraw();
 }
 Canvas.prototype.redraw = function () // Canvas class's redraw method.
 {
-	this.ctx.clearRect(0, 0, canvasX, canvasY);
-	for (var x in game.blocks) // Redraw all static blocks.
-	{
-		if (game.blocks[x])
-		{
-			game.blocks[x].redraw();
-		}
-	}
-	// Redraw level-based game components.
-	for (var x in game.bubbles) // Redraw all user generated bubbles.
+	for (var x in game.bubbles)
 	{
 		if (game.bubbles[x])
 		{
-			game.bubbles[x].redraw();
+			game.bubbles[x].calculate();
 		}
+	}
+	this.calcCount++;
+	if (this.calcCount === msPerRender)
+	{
+		this.ctx.clearRect(0, 0, canvasX, canvasY);
+		for (var x in game.blocks) // Redraw all static blocks.
+		{
+			if (game.blocks[x])
+			{
+				game.blocks[x].redraw();
+			}
+		}
+		// Redraw level-based game components.
+		for (var x in game.bubbles) // Redraw all user generated bubbles.
+		{
+			
+			if (game.bubbles[x])
+			{
+				game.bubbles[x].redraw();
+			}
+		}
+		if (game.asplodeBubble)
+		{
+			game.asplodeBubble.asplode();
+		}
+		this.calcCount = 0;
 	}
 }
 
@@ -199,7 +222,7 @@ Bubble.prototype.create = function (x, y) // Create bubble.
 	this.moving = true;
 	this.calculate();
 }
-Bubble.prototype.redraw = function () // Bubble class's redraw method.
+Bubble.prototype.calculate = function () // Bubble class's calculation method for next position.
 {
 	for (var x in game.bubbles)
 	{
@@ -208,35 +231,6 @@ Bubble.prototype.redraw = function () // Bubble class's redraw method.
 			this.combine(game.bubbles[x]);
 		}
 	}
-	this.calculate();
-	if (this.y <= 50)
-	{
-		this.destruct();
-	}
-	canvas.ctx.beginPath();
-	canvas.ctx.strokeStyle = "LightSeaGreen";
-	canvas.ctx.arc((this.moving ? this.x+Math.sqrt(this.r)/2-Math.random()*Math.sqrt(this.r) : this.x), this.y, this.r, 0, 2*Math.PI);
-	canvas.ctx.stroke();
-	canvas.ctx.fillStyle = "LightSeaGreen";
-	canvas.ctx.font = "10px Tahoma, Geneva, sans-serif";
-	canvas.ctx.textAlign = "center";
-	canvas.ctx.textBaseline = "middle";
-	canvas.ctx.fillText(this.weight, this.x, this.y);
-}
-Bubble.prototype.combine = function (bubbleObject) // Bubble class's combine method for the case of two bubbles touching.
-{
-	if ((this.weight <= bubbleObject.weight) && (Math.sqrt(Math.pow(this.x-bubbleObject.x, 2)+Math.pow(this.y-bubbleObject.y, 2)) <= this.r+bubbleObject.r))
-	{
-		var bubbleObjectWeight = bubbleObject.weight;
-		bubbleObject.weight += this.weight;
-		bubbleObject.y += Math.sqrt(Math.pow(bubbleRadius, 2)*this.weight);
-		bubbleObject.x = (this.x*this.weight+bubbleObject.x*bubbleObjectWeight)/(this.weight+bubbleObjectWeight);
-		this.destruct();
-		bubbleObject.redraw();
-	}
-}
-Bubble.prototype.calculate = function () // Bubble class's calculation method for next position.
-{
 	this.r = Math.sqrt(Math.pow(bubbleRadius, 2)*this.weight);
 	var returnVal = false;
 	for (var x in game.blocks)
@@ -253,6 +247,57 @@ Bubble.prototype.calculate = function () // Bubble class's calculation method fo
 	{
 		this.y = this.y-1.5;
 		this.moving = true;
+	}
+	if (this.y <= 50)
+	{
+		this.destruct();
+	}
+	if (this.weight >= maxBubbleWeight)
+	{
+		this.r *= 2;
+		this.y += this.r/4;
+		game.asplodeBubble = this;
+		this.destruct();
+	}
+}
+Bubble.prototype.combine = function (bubbleObject) // Bubble class's combine method for the case of two bubbles touching.
+{
+	if ((this.weight <= bubbleObject.weight) && (Math.sqrt(Math.pow(this.x-bubbleObject.x, 2)+Math.pow(this.y-bubbleObject.y, 2)) <= this.r+bubbleObject.r))
+	{
+		var bubbleObjectWeight = bubbleObject.weight;
+		bubbleObject.weight += this.weight;
+		bubbleObject.y += Math.sqrt(Math.pow(bubbleRadius, 2)*this.weight);
+		bubbleObject.x = (this.x*this.weight+bubbleObject.x*bubbleObjectWeight)/(this.weight+bubbleObjectWeight);
+		this.destruct();
+		bubbleObject.redraw();
+	}
+}
+Bubble.prototype.redraw = function () // Bubble class's redraw method.
+{
+	canvas.ctx.beginPath();
+	canvas.ctx.strokeStyle = "LightSeaGreen";
+	canvas.ctx.arc((this.moving ? this.x+Math.sqrt(this.r)/2-Math.random()*Math.sqrt(this.r) : this.x), this.y, this.r, 0, 2*Math.PI);
+	canvas.ctx.stroke();
+	canvas.ctx.fillStyle = "LightSeaGreen";
+	canvas.ctx.font = "10px Tahoma, Geneva, sans-serif";
+	canvas.ctx.textAlign = "center";
+	canvas.ctx.textBaseline = "middle";
+	canvas.ctx.fillText(this.weight, this.x, this.y);
+}
+Bubble.prototype.asplode = function ()
+{
+	if (this.r >= 3)
+	{
+		this.r -= 1.5;
+		this.y -= 1.5;
+		canvas.ctx.beginPath();
+		canvas.ctx.strokeStyle = "LightSkyBlue";
+		canvas.ctx.arc(this.x+Math.sqrt(this.r)-2*Math.random()*Math.sqrt(this.r), this.y, this.r, 0, 2*Math.PI);
+		canvas.ctx.stroke();
+	}
+	else if (this.r < 2)
+	{
+		game.asplodeBubble = null;
 	}
 }
 Bubble.prototype.destruct = function () // Bubble class's destruct method.
